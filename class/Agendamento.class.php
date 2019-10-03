1e21eb66049fb8f7ce08edd6ef0fc993758d6f34
LucasThomaz139 <lucas.thomaz.05.09@gmail.com>
2019-10-02 22:16:26 -0300

    acrescentei métodos(listar, verificador, salvar, excluir) na class Agendamento

--- class/Agendamento.class.php
+++ class/Agendamento.class.php
@@ -71,6 +71,7 @@ class Agendamento {
                 unset($conecta);
             }
         }
+    }
         function veri_adicionar($agenda){
          $conecta;
 
@@ -106,6 +107,139 @@ class Agendamento {
             }
         }
     }
+    function lista($agendar) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql ="SELECT agendamento.*,usuario.nome as cadastros FROM agendamento INNER JOIN usuario ON agendamento.idagendameto=usuario.idusuario ";
+            $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":idagendamento", $agendar->getIdagendamento());
+            $preparedStatment->bindValue(":cadastro_usuario", $agendar->getCadastro_usuario());
+            $preparedStatment->bindValue(":data", $agendar->getData());
+            $preparedStatment->bindValue(":descricao", $agendar->getDescricao());
+            $preparedStatment->bindValue(":hora", $agendar->getHora());
+            $preparedStatment->bindValue(":agenda", $agendar->getAgenda());
+            $prepara->execute();
+            $conecta->commit();
+            $lista = null;
+            while($pegando = $prepara->fetch(PDO::FETCH_ASSOC)){
+                $listando=new Cadastro();
+                $listando->idagendamento=$pegando['idagendamento'];
+                $listando->cadastro_usuario=$pegando['cadastros'];
+                $listando->data=$pegando['data'];
+                $listando->descricao=$pegando['descricao'];
+                $listando->agenda=$pegando['agenda'];
+                $lista[] = $listando;                
+            }
+            return $lista;
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+    }
+     function verificador($agendar){
+        
+         $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "SELECT*FROM agendamento WHERE idagendamento=:idagendamento";
+            $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":idagendamento", $agendar->getIdagendamento());
+            $prepara->bindValue(":cadastro_usuario", $agenda->getCadastro_usuario());
+            $prepara->bindValue(":data", $agenda->getData());
+            $prepara->bindValue(":descricao", $agenda->getDescricao());
+            $prepara->bindValue(":hora", $agenda->getHora());
+            $prepara->bindValue(":agenda", $agenda->getAgenda());
+            $prepara->execute();
+            $conecta->commit();
+            $lista = null;
+            if($pegando=$prepara->fetch(PDO::FETCH_ASSOC)){
+                $listando=new Cadastro();
+                $listando->idagendamento=$pegando['idagendamento'];
+                $listando->cadastro_usuario=$pegando['cadastro_usuario'];
+                $listando->data=$pegando['data'];
+                $listando->descricao=$pegando['descricao'];
+                $listando->agenda=$pegando['agenda'];
+                $lista=$listando;
+            }
+            return $lista;
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+     }
+     
+            }
+          function salvar($agendar){
+         $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "UPDATE agendamento SET  cadastro_usuario=:cadastro,data=:data,descricao=:descricao,agenda=:agenda WHERE idagendamento =:idagendamento";
+            //print_r($sql);
+           $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":idagendamento", $agendar->getIdagendamento());
+            $prepara->bindValue(":cadastro_usuario", $agenda->getCadastro_usuario());
+            $prepara->bindValue(":data", $agenda->getData());
+            $prepara->bindValue(":descricao", $agenda->getDescricao());
+            $prepara->bindValue(":hora", $agenda->getHora());
+            $prepara->bindValue(":agenda", $agenda->getAgenda());
+            $prepara->execute();
+             $conecta->commit();
+            
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            echo $exc->getMessage();
+            
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+        
+    }
+        
+     public function excluir($agendar) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "DELETE*FROM agendamento WHERE idagendamento=:idagendamento";
+            $prepara=$conecta->prepare($sql);
+            $prepara->bindValue(":idagendamento",$agendar->getIdagendamento());
+            $prepara->execute();
+            $conecta->commit();
+            
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            echo $exc->getMessage();
+            
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+    }
+}
+    
 
-}
-}
+
