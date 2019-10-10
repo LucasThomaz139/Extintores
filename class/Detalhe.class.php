1639c91af7acc55b14be2cab338756f697dd101f
LucasThomaz139 <lucas.thomaz.05.09@gmail.com>
2019-10-09 21:10:53 -0300

    Alterando o arquivo detalhe na pasta class alterando (adicionar) e adicionando os metodos(listar,verificar,salvar,excluir)

--- class/Detalhe.class.php
+++ class/Detalhe.class.php
@@ -63,22 +63,20 @@ class Detalhe {
     function setValor($valor) {
         $this->valor = $valor;
     }
-    function adicionar($datalhe){
+    function adicionar($detalhe){
         $conectar;
         try {
             $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
             $conecta->beginTransaction();
-            $sql = "INSERT INTO detalhe(produto_idprodutos,servico_idservico,quantidade,detalhe,valor) VALUES(:nome,:telefonetra,:telefonepe,:razaosocial,:cnpjt,:endereco,:email,:senha)";
+            $sql = "INSERT INTO detalhe(produto_idprodutos,servico_idservico,agendamento_idagendamento,quantidade,detalhe,valor) VALUES(:produto_idprodutos,:servico_idservico,:agendamento_idagendamento,:quantidade,:detalhe,:valor)";
             //print_r($sql);
             $prepara =$conecta->prepare($sql);
-            $prepara->bindValue(":nome",$cadastro->getNome());
-            $prepara->bindValue(":telefonetra",$cadastro->getTelefonetra());
-            $prepara->bindValue(":telefonepe",$cadastro->getTelefonepe());
-            $prepara->bindValue(":razaosocial",$cadastro->getRazaosocial());
-            $prepara->bindValue(":cnpjt",$cadastro->getCnpjt());
-            $prepara->bindValue(":endereco",$cadastro->getEndereco());
-            $prepara->bindValue(":email",$cadastro->getEmail());
-            $prepara->bindValue(":senha",$cadastro->getSenha());
+            $prepara->bindValue(":produto_idprodutos",$detalhe->getProduto_idprodutos());
+            $prepara->bindValue(":servico_idservico",$detalhe->getServico_idservico());
+            $prepara->bindValue(":agendamento_idagendamento",$detalhe->getAgendamento_idagendamento());
+            $prepara->bindValue(":quantidade",$detalhe->getQuantidade());
+            $prepara->bindValue(":detalhe",$detalhe->getDetalhe());
+            $prepara->bindValue(":valor",$detalhe->getValor());
             $prepara->execute();
             $conecta->commit();
         } catch (PDOException $exc) {
@@ -93,6 +91,143 @@ class Detalhe {
             }
         }
     }
-
+     function lista($detalhe) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql ="SELECT detalhe.*,produtos.nome as produto,servico.nome as servico,agendamento.data as agendamento FROM detalhe INNER JOIN produto ON detalhe.iddetalhe=produto.idprodutos INNER JOIN servico ON detalhe.iddetalhe=servico.idservico INNER JOIN agendamento ON detalhe.iddetalhe=agendamento.idagendamento";
+            $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":iddetalhe", $detalhe->getIdDetalhe());
+            $prepara->bindValue(":produto_idprodutos",$detalhe->getProduto_idprodutos());
+            $prepara->bindValue(":servico_idservico",$detalhe->getServico_idservico());
+            $prepara->bindValue(":agendamento_idagendamento",$detalhe->getAgendamento_idagendamento());
+            $prepara->bindValue(":quantidade",$detalhe->getQuantidade());
+            $prepara->bindValue(":detalhe",$detalhe->getDetalhe());
+            $prepara->bindValue(":valor",$detalhe->getValor());
+            $prepara->execute();
+            $conecta->commit();
+            $lista = null;
+            while($pegando = $prepara->fetch(PDO::FETCH_ASSOC)){
+                $listando=new Cadastro();
+                $listando->iddetalhe=$pegando['idagendamento'];
+                $listando->produto_idprodutos=$pegando['produto'];
+                $listando->servico_idservico=$pegando['servico'];
+                $listando->agendamento_idagendamento=$pegando['agendamento'];
+                $listando->quantidade=$pegando['quantidade'];
+                $listando->detalhe=$pegando['detalhe'];
+                $listando->valor=$pegando['valor'];
+                $lista[] = $listando;                
+            }
+            return $lista;
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+    }
+     function verificador($detalhe){
+        
+         $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "SELECT detalhe.*,produtos.nome as produto,servico.nome as servico,agendamento.data as agendamento FROM detalhe INNER JOIN produto ON detalhe.iddetalhe=produto.idprodutos INNER JOIN servico ON detalhe.iddetalhe=servico.idservico INNER JOIN agendamento ON detalhe.iddetalhe=agendamento.idagendamento";
+            $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":iddetalhe", $detalhe->getIdDetalhe());
+            $prepara->bindValue(":produto_idprodutos",$detalhe->getProduto_idprodutos());
+            $prepara->bindValue(":servico_idservico",$detalhe->getServico_idservico());
+            $prepara->bindValue(":agendamento_idagendamento",$detalhe->getAgendamento_idagendamento());
+            $prepara->bindValue(":quantidade",$detalhe->getQuantidade());
+            $prepara->bindValue(":detalhe",$detalhe->getDetalhe());
+            $prepara->bindValue(":valor",$detalhe->getValor());
+            $prepara->execute();
+            $conecta->commit();
+            $lista = null;
+            if($pegando = $prepara->fetch(PDO::FETCH_ASSOC)){
+                $listando=new Cadastro();
+                 $listando->iddetalhe=$pegando['idagendamento'];
+                $listando->produto_idprodutos=$pegando['produto'];
+                $listando->servico_idservico=$pegando['servico'];
+                $listando->agendamento_idagendamento=$pegando['agendamento'];
+                $listando->quantidade=$pegando['quantidade'];
+                $listando->detalhe=$pegando['detalhe'];
+                $listando->valor=$pegando['valor'];
+                $lista = $listando;                
+            }
+            return $lista;
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+     }
+     
+            }
+          function salvar($agendar){
+         $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "UPDATE  SET  produto_idprodutos=:produto_idprodutos,servico_idservico=:servico_idservico,agendamento_idagendamento=:agendamento_idagendamento,quantidade=:quantidade,detalhe=:detalhe,valor=:valor WHERE iddetalhe =:iddetalhe";
+            //print_r($sql);
+           $prepara = $conecta->prepare($sql);
+           $prepara->bindValue(":iddetalhe", $detalhe->getIdDetalhe());
+            $prepara->bindValue(":produto_idprodutos",$detalhe->getProduto_idprodutos());
+            $prepara->bindValue(":servico_idservico",$detalhe->getServico_idservico());
+            $prepara->bindValue(":agendamento_idagendamento",$detalhe->getAgendamento_idagendamento());
+            $prepara->bindValue(":quantidade",$detalhe->getQuantidade());
+            $prepara->bindValue(":detalhe",$detalhe->getDetalhe());
+            $prepara->bindValue(":valor",$detalhe->getValor());
+            $prepara->execute();
+             $conecta->commit();
+            
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            echo $exc->getMessage();
+            
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+          }
+          
+            }
+        public function excluir($detalhe) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "DELETE*FROM agendamento WHERE idagendamento=:idagendamento";
+            $prepara=$conecta->prepare($sql);
+            $prepara->bindValue(":iddetalhe",$detalhe->getIddetalhe());
+            $prepara->execute();
+            $conecta->commit();
+            
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            echo $exc->getMessage();
+            
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+        }
 
 }
