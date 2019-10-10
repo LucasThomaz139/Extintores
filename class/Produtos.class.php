0fd7494de6799d19d6bb5019acc83294cbad4852
LucasThomaz139 <lucas.thomaz.05.09@gmail.com>
2019-10-09 22:49:13 -0300

    alterei o arquivo Produto adicionei metodo(adicionar,listar, verificar, salvar, excluir)

--- class/Produtos.class.php
+++ class/Produtos.class.php
@@ -72,6 +72,178 @@ class Produtos {
         $this->imagem = $imagem;
     }
 
-
+function adicionar($produtos) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "INSERT INTO produto(nome,valor,tipo,descricao,quantidade,status,imagem) VALUES(:nome,:valor,:tipo,:descricao,:quantidade,:status,:imagem)";
+            $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":nome", $produtos->getNome());
+            $prepara->bindValue(":valor", $produtos->getValor());
+            $prepara->bindValue(":tipo", $produtos->getTipo());
+            $prepara->bindValue(":descricao", $produtos->getDescricao());
+            $prepara->bindValue(":quantidade", $produtos->getQuantidade());
+            $prepara->bindValue(":status", $produtos->getStatus());
+            $prepara->bindValue(":imagem", $produtos->getImagem());
+            $prepara->execute();
+            $conecta->commit();
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+    }
+        
+    function lista($produtos) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql ="SELECT*FROM produto WHERE idproduto=:idproduto ";
+            $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":idproduto", $produtos->getIdproduto());
+            $prepara->bindValue(":nome", $produtos->getNome());
+            $prepara->bindValue(":valor", $produtos->getValor());
+            $prepara->bindValue(":tipo", $produtos->getTipo());
+            $prepara->bindValue(":descricao", $produtos->getDescricao());
+            $prepara->bindValue(":quantidade", $produtos->getQuantidade());
+            $prepara->bindValue(":status", $produtos->getStatus());
+            $prepara->bindValue(":imagem", $produtos->getImagem());
+            $prepara->execute();
+            $conecta->commit();
+            $lista = null;
+            while($pegando = $prepara->fetch(PDO::FETCH_ASSOC)){
+                $listando=new Cadastro();
+                $listando->idproduto=$pegando['idproduto'];
+                $listando->nome=$pegando['nome'];
+                $listando->valor=$pegando['valor'];
+                $listando->tipo=$pegando['tipo'];
+                $listando->descricao=$pegando['descricao'];
+                $listando->quantidade=$pegando['quantidade'];
+                $listando->status=$pegando['status'];
+                $listando->imagem=$pegando['imagem'];
+                $lista[] = $listando;                
+            }
+            return $lista;
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+    }
+     function verificador($produtos){
+        
+         $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+             $sql ="SELECT*FROM produto WHERE idproduto=:idproduto ";
+            $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":idproduto", $produtos->getIdproduto());
+            $prepara->bindValue(":nome", $produtos->getNome());
+            $prepara->bindValue(":valor", $produtos->getValor());
+            $prepara->bindValue(":tipo", $produtos->getTipo());
+            $prepara->bindValue(":descricao", $produtos->getDescricao());
+            $prepara->bindValue(":quantidade", $produtos->getQuantidade());
+            $prepara->bindValue(":status", $produtos->getStatus());
+            $prepara->bindValue(":imagem", $produtos->getImagem());
+            $prepara->execute();
+            $conecta->commit();
+            $lista = null;
+            if($pegando = $prepara->fetch(PDO::FETCH_ASSOC)){
+                $listando=new Cadastro();
+                $listando->idproduto=$pegando['idproduto'];
+                $listando->nome=$pegando['nome'];
+                $listando->valor=$pegando['valor'];
+                $listando->tipo=$pegando['tipo'];
+                $listando->descricao=$pegando['descricao'];
+                $listando->quantidade=$pegando['quantidade'];
+                $listando->status=$pegando['status'];
+                $listando->imagem=$pegando['imagem'];
+                $lista = $listando;                
+            }
+            return $lista;
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+     }
+     
+            }
+          function salvar($produtos){
+         $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "UPDATE produtos SET  nome=:nome,valor=:valor,tipo=:tipo,descricao=:descricao,quantidade=:quantidade,status=:status,imagem=:imagem WHERE idproduto =:idproduto";
+            //print_r($sql);
+           $prepara = $conecta->prepare($sql);
+           $prepara->bindValue(":idproduto", $produtos->getIdproduto());
+            $prepara->bindValue(":nome", $produtos->getNome());
+            $prepara->bindValue(":valor", $produtos->getValor());
+            $prepara->bindValue(":tipo", $produtos->getTipo());
+            $prepara->bindValue(":descricao", $produtos->getDescricao());
+            $prepara->bindValue(":quantidade", $produtos->getQuantidade());
+            $prepara->bindValue(":status", $produtos->getStatus());
+            $prepara->bindValue(":imagem", $produtos->getImagem());
+            $prepara->execute();
+             $conecta->commit();
+            
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            echo $exc->getMessage();
+            
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+        
+    }
+        
+     public function excluir($produtos) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "DELETE*FROM agendamento WHERE idagendamento=:idagendamento";
+            $prepara=$conecta->prepare($sql);
+            $prepara->bindValue(":idproduto",$produtos->getIdproduto());
+            $prepara->execute();
+            $conecta->commit();
+            
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            echo $exc->getMessage();
+            
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+    }
 
 }
