065c7b0c2f2ef2220655e574807a2cbe2990fb4d
LucasThomaz139 <lucas.thomaz.05.09@gmail.com>
2019-10-09 22:13:36 -0300

    alterei o arquivo mensagem da pasta class adicionando os metodos (adicionar,listar,verificar,salvar,excluir)

--- class/Mensagem.class.php
+++ class/Mensagem.class.php
@@ -28,6 +28,149 @@ class Mensagem {
         $this->avaliacao = $avaliacao;
     }
 
-
+ function adicionar($mensagens) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "INSERT INTO mensagem(mensagem,avaliacao) VALUES(:mensagem,:avaliacao)";
+            $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":mensagem", $mensagens->getMensagem());
+            $prepara->bindValue(":avaliacao", $mensagens->getAvaliacao());
+            
+            $prepara->execute();
+            $conecta->commit();
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+    }
+        
+    function lista($mensagens) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql ="SELECT*FROM mensagem WHERE idmensagem=:idmensagem ";
+            $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":idmensagem", $mensagens->getIdmensagem());
+             $prepara->bindValue(":mensagem", $mensagens->getMensagem());
+            $prepara->bindValue(":avaliacao", $mensagens->getAvaliacao());
+            $prepara->execute();
+            $conecta->commit();
+            $lista = null;
+            while($pegando = $prepara->fetch(PDO::FETCH_ASSOC)){
+                $listando=new Cadastro();
+                $listando->idmensagem=$pegando['idmensagem'];
+                $listando->mensagem=$pegando['mensagem'];
+                $listando->avaliacao=$pegando['avaliacao'];
+                $lista[] = $listando;                
+            }
+            return $lista;
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+    }
+     function verificador($mensagens){
+        
+         $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "SELECT*FROM mensagem WHERE idmensagem=:idmensagem";
+            $prepara = $conecta->prepare($sql);
+             $prepara->bindValue(":idmensagem", $mensagens->getIdmensagem());
+             $prepara->bindValue(":mensagem", $mensagens->getMensagem());
+            $prepara->bindValue(":avaliacao", $mensagens->getAvaliacao());
+            $prepara->execute();
+            $conecta->commit();
+            $lista = null;
+            while($pegando = $prepara->fetch(PDO::FETCH_ASSOC)){
+                $listando=new Cadastro();
+                $listando->idmensagem=$pegando['idmensagem'];
+                $listando->mensagem=$pegando['mensagem'];
+                $listando->avaliacao=$pegando['avaliacao'];
+                $lista[] = $listando;                
+            }
+            return $lista;
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+     }
+     
+            }
+          function salvar($agendar){
+         $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "UPDATE agendamento SET  mensagem=:mensagem,avaliacao=:avaliacao WHERE idmensagem =:idmensagem";
+            //print_r($sql);
+           $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":idmensagem", $mensagens->getIdmensagem());
+            $prepara->bindValue(":mensagem", $mensagens->getMensagem());
+            $prepara->bindValue(":avaliacao", $mensagens->getAvaliacao());
+            $prepara->execute();
+             $conecta->commit();
+            
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            echo $exc->getMessage();
+            
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+        
+    }
+        
+     public function excluir($mensagens) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "DELETE*FROM mensagem WHERE idmensagem=:idmensagem";
+            $prepara=$conecta->prepare($sql);
+            $prepara->bindValue(":idmensagem",$mensagens->getIdmensagem());
+            $prepara->execute();
+            $conecta->commit();
+            
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            echo $exc->getMessage();
+            
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+    }
 
 }
