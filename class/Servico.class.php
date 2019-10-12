66e169c98f63d658e71c44c9e4f3c078e13766f1
LucasThomaz139 <lucas.thomaz.05.09@gmail.com>
2019-10-12 11:42:30 -0300

    alterei o arquivo Servico adicionei metodo(adicionar,listar, verificar, salvar, excluir)

--- class/Servico.class.php
+++ class/Servico.class.php
@@ -36,6 +36,155 @@ class Servico {
     function setDescricao($descricao) {
         $this->descricao = $descricao;
     }
+    function adicionar($servicos) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "INSERT INTO servico(nome,valor,descricao) VALUES(:nome,:valor,:descricao)";
+            $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":nome", $servicos->getNome());
+            $prepara->bindValue(":valor", $servicos->getValor());
+            $prepara->bindValue(":descricao", $servicos->getDescricao());
+            $prepara->execute();
+            $conecta->commit();
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+    }
+        
+    function lista($servicos) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql ="SELECT*FROM servico WHERE idservico=:idservico ";
+            $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":idservico", $servicos->getIdservico());
+            $prepara->bindValue(":nome", $servicos->getNome());
+            $prepara->bindValue(":valor", $servicos->getValor());
+            $prepara->bindValue(":descricao", $servicos->getDescricao());
+            $prepara->execute();
+            $conecta->commit();
+            $lista = null;
+            while($pegando = $prepara->fetch(PDO::FETCH_ASSOC)){
+                $listando=new Cadastro();
+                $listando->idproduto=$pegando['idproduto'];
+                $listando->nome=$pegando['nome'];
+                $listando->valor=$pegando['valor'];
+                $listando->descricao=$pegando['descricao'];
+                $lista[] = $listando;                
+            }
+            return $lista;
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+    }
+     function verificador($servicos){
+        
+         $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+             $sql ="SELECT*FROM servico WHERE idservico=:idservico ";
+            $prepara = $conecta->prepare($sql);
+            $prepara->bindValue(":idservico", $servicos->getIdservico());
+            $prepara->bindValue(":nome", $servicos->getNome());
+            $prepara->bindValue(":valor", $servicos->getValor());
+            $prepara->bindValue(":descricao", $servicos->getDescricao());
+            $prepara->execute();
+            $conecta->commit();
+            $lista = null;
+            if($pegando = $prepara->fetch(PDO::FETCH_ASSOC)){
+                $listando=new Cadastro();
+                $listando->idproduto=$pegando['idproduto'];
+                $listando->nome=$pegando['nome'];
+                $listando->valor=$pegando['valor'];
+                $listando->descricao=$pegando['descricao'];
+                $lista = $listando;                
+            }
+            return $lista;
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            PRINT($exc->getMessage());
+            return FALSE;
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+     }
+     
+            }
+          function salvar($servicos){
+         $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "UPDATE servico SET  nome=:nome,valor=:valor,descricao=:descricao WHERE idservico =:idservico";
+            //print_r($sql);
+           $prepara = $conecta->prepare($sql);
+           $prepara->bindValue(":idservico", $servicos->getIdservico());
+            $prepara->bindValue(":nome", $servicos->getNome());
+            $prepara->bindValue(":valor", $servicos->getValor());
+            $prepara->bindValue(":descricao", $servicos->getDescricao());
+            $prepara->execute();
+             $conecta->commit();
+            
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            echo $exc->getMessage();
+            
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+        
+    }
+        
+     public function excluir($servicos) {
+        $conecta;
+        try {
+            $conecta = new PDO('mysql:host=127.0.0.1;dbname=extintores', 'root', '');
+            $conecta->beginTransaction();
+            $sql = "DELETE*FROM servico WHERE idservico=:idservico";
+            $prepara=$conecta->prepare($sql);
+            $prepara->bindValue(":idservico",$servicos->getIdservico());
+            $prepara->execute();
+            $conecta->commit();
+            
+        } catch (PDOException $exc) {
+            if ((isset($conecta)) && ($conecta->inTransaction())) {
+                $conecta->rollBack();
+            }
+            echo $exc->getMessage();
+            
+        } finally {
+            if (isset($conecta)) {
+                unset($conecta);
+            }
+        }
+    }
 
 
 }
