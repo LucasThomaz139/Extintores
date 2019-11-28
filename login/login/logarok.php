d2ac6fc2b69dd0f95856d472109d9c3c9ba7df9c
LucasThomaz139 <56095406+LucasThomaz139@users.noreply.github.com>
2019-11-28 00:12:55 -0300

    Add files via upload

---
+++ login/login/logarok.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+include_once '../class/Cadastro.class.php';
+$us= new Cadastro();
+$us->setEmail($_POST['email']);
+$us->setSenha($_POST['senha']);
+
+$retorno=$us->loginadm($us);
+if ($retorno) {
+    $_SESSION['idusuario']=$retorno['idusuario'];
+   $_SESSION['nome']=$retorno['nome'];
+   $_SESSION['login']=true;
+   $_SESSION['administrador']=true;
+   header("location:../produto/listar.php");
+}
+else{
+    echo"a senha esta errada ou email errado";
+      header("location:../login/senha.php");
+}
+
+
